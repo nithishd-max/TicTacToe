09401250a32b39f4eb7f236a2c5396a10cbb2d2d
nithishdnr <nithishdnr3@gmail.com>
2020-08-10 21:23:39 +0530

    Updates

---
+++ precache-manifest.5d1d64aa63e4210e02722525a6d6c095.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6359f6c580943eef8efafe83fa342366",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "a58937572e0a14fb3246",
+    "url": "/TicTacToe/static/css/2.28eb3bf6.chunk.css"
+  },
+  {
+    "revision": "2d9230f1b4400359a719",
+    "url": "/TicTacToe/static/css/main.10f154a7.chunk.css"
+  },
+  {
+    "revision": "a58937572e0a14fb3246",
+    "url": "/TicTacToe/static/js/2.8674283e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TicTacToe/static/js/2.8674283e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9230f1b4400359a719",
+    "url": "/TicTacToe/static/js/main.a022ce8b.chunk.js"
+  },
+  {
+    "revision": "c3336fe6ce6e56470618",
+    "url": "/TicTacToe/static/js/runtime-main.2b09fe6a.js"
+  }
+]);
